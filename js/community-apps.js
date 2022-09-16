8102a6744782654104d2facc839786ec34c2c5d5
Bunz <66bunz@gmail.com>
2022-09-16 19:37:24 +0200

    added opengraph cards meta tags

--- js/community-apps.js
+++ js/community-apps.js
@@ -3,51 +3,71 @@
 
 const appsList = document.getElementById("appsList");
 
-/* Get apps from JSON */
 class Apps {
   async Items() {
     try {
-      let result = await fetch('/data/community_apps/community_apps.json')
+      console.log('start')
+      let result = await fetch('https://raw.githubusercontent.com/Droptop-Four/GlobalData/main/data/community_apps/community_apps.json')
+      console.log('end')
       let data = await result.json()
-      // return data
+      console.log(data)
 
-      /* destructuring data */
       let appsItems = data.apps
       appsItems = appsItems.map(item => {
-        const { id, name, author, author_link, desc, official_link, direct_download_link, secondary_link, image } = item.app
+        const { id, name, author, author_link, desc, official_link, direct_download_link, secondary_link, image_url } = item.app
 
-        return { id, name, author, author_link, desc, official_link, direct_download_link, secondary_link, image }
+        return { id, name, author, author_link, desc, official_link, direct_download_link, secondary_link, image_url }
       })
       return appsItems
 
     } catch (error) {
       console.log(error)
+      console.log('test')
     }
   }
 }
 
-/* Display apps */
 class DisplayApps {
   displayApps(apps) {
-    //console.log(basket)
     let result = ""
     apps.forEach((item) => {
-      result += `
-        <li>
-          <div class="app-card" id="${item.id}">
-            <div class="app-card-container">
-              <img class="app-card-image" src="${item.image}" alt="${item.name} image">
-              <h3 class="app-card-name">${item.name}</h3>
-              <p class="app-card-author">Created by <a class="app-card-author-link" href="${item.author_link}">${item.author}</a></p>
-              <p class="app-card-desc">${item.desc}</p>
-              <div class="app-card-buttons">
-                  <button class="app-card-button" onclick="location.href='${item.official_link}'" type="button">See on Github</button>
-                  <button class="app-card-button" onclick="location.href='${item.direct_download_link}'" type="button">Download</button>
-              </div>
-            </div>  
-          </div>
-        </li>
-        `})
+      if (item.author_link == '#') {
+        result += `
+          <li>
+            <div class="app-card" id="${item.id}">
+              <div class="app-card-container">
+                <img class="app-card-image" src="${item.image_url}" alt="${item.name} image">
+                <h3 class="app-card-name">${item.name}</h3>
+                <p class="app-card-author">Created by <a class="app-card-author-link">${item.author}</a></p>
+                <p class="app-card-desc">${item.desc}</p>
+                <div class="app-card-buttons">
+                    <button class="app-card-button" onclick="location.href='${item.official_link}'" type="button">See on Github</button>
+                    <button class="app-card-button" onclick="location.href='${item.direct_download_link}'" type="button">Download</button>
+                </div>
+              </div>  
+            </div>
+          </li>
+          `
+      } else {
+        result += `
+          <li>
+            <div class="app-card" id="${item.id}">
+              <div class="app-card-container">
+                <img class="app-card-image" src="${item.image_url}" alt="${item.name} image">
+                <h3 class="app-card-name">${item.name}</h3>
+                <p class="app-card-author">Created by <a class="app-card-author-link" href="${item.author_link}">${item.author}</a></p>
+                <p class="app-card-desc">${item.desc}</p>
+                <div class="app-card-buttons">
+                    <button class="app-card-button" onclick="location.href='${item.official_link}'" type="button">See on Github</button>
+                    <button class="app-card-button" onclick="location.href='${item.direct_download_link}'" type="button">Download</button>
+                </div>
+              </div>  
+            </div>
+          </li>
+          `
+        }
+      }
+          )
     appsList.innerHTML = result
   }
 }
@@ -58,4 +78,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
   apps.Items().then(apps => displayapps.displayApps(apps))
 })
-
