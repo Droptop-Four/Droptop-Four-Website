77ed5803e1a37ca8355a534303cef327a2a2096d
Bunz <66331265+66Bunz@users.noreply.github.com>
2022-09-22 22:22:09 +0200

    Add scroll to top button for mobiles

--- js/main.js
+++ js/main.js
@@ -74,3 +74,21 @@ function AgentAlert(version) {
 }
 
 
+let mybutton = document.getElementById("scroll-to-top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
