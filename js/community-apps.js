2f8c853d75d7d7f847a1fe885f084b5e95f848ea
Bunz <66bunz@gmail.com>
2023-10-22 16:44:37 +0200

    Added modals for Readmes

--- js/community-apps.js
+++ js/community-apps.js
@@ -10,6 +10,13 @@ let id_query = params.id;
 
 const appsList = document.getElementById('appsList');
 
+function convertToRawGitHubURL(githubURL) {
+	const rawURL = githubURL
+		.replace('github.com', 'raw.githubusercontent.com')
+		.replace('/blob/', '/');
+	return rawURL;
+}
+
 class Apps {
 	async Items() {
 		try {
@@ -20,7 +27,7 @@ class Apps {
 
 			let appsItems = data.apps;
 
-			appsItems = appsItems.map((item) => {
+			const fetchAppsPromises = appsItems.map(async (item) => {
 				const {
 					id,
 					name,
@@ -35,23 +42,71 @@ class Apps {
 					hidden,
 				} = item.app;
 
-				return {
-					id,
-					name,
-					author,
-					author_link,
-					desc,
-					version,
-					official_link,
-					direct_download_link,
-					secondary_link,
-					image_url,
-					hidden,
-				};
+				if (item.app.official_link == '') {
+					const readmeExists = false;
+					return {
+						id,
+						name,
+						author,
+						author_link,
+						desc,
+						version,
+						official_link,
+						direct_download_link,
+						secondary_link,
+						image_url,
+						hidden,
+						readmeExists,
+					};
+				} else {
+					const rawBaseURL = convertToRawGitHubURL(
+						`${item.app.official_link}`
+					);
+
+					try {
+						const response = await fetch(
+							`${rawBaseURL}/main/README.md`
+						);
+						const readmeExists = response.status === 200;
+						return {
+							id,
+							name,
+							author,
+							author_link,
+							desc,
+							version,
+							official_link,
+							direct_download_link,
+							secondary_link,
+							image_url,
+							hidden,
+							readmeExists,
+						};
+					} catch (error) {
+						const readmeExists = false;
+						return {
+							id,
+							name,
+							author,
+							author_link,
+							desc,
+							version,
+							official_link,
+							direct_download_link,
+							secondary_link,
+							image_url,
+							hidden,
+							readmeExists,
+						};
+					}
+				}
 			});
-			return appsItems;
+
+			const appsItemsWithReadme = await Promise.all(fetchAppsPromises);
+
+			return appsItemsWithReadme;
 		} catch (error) {
-			console.log(error);
+			// console.log(error);
 		}
 	}
 }
@@ -80,23 +135,46 @@ class DisplayApps {
             </div>
             `;
 					} else {
-						result += `
-            <div>
-              <div class="app-card" id="${item.id}">
-                <div class="app-card-container">
-                  <a><img href="javascript:void(0)" onclick="openImageModal('${item.image_url}', '${item.name}'); return false" class="app-card-image" src="${item.image_url}" alt="${item.name} image"></a>
-                  <h3 class="app-card-name">${item.name}</h3>
-                  <p class="app-card-version">v${item.version}</p>
-                  <p class="app-card-author">Created by <a class="app-card-author-link">${item.author}</a></p>
-                  <p class="app-card-desc">${item.desc}</p>
-                  <div class="app-card-buttons">
-                      <a class="app-card-button bold" href="${item.direct_download_link}">Download</a>
-                      <a class="app-card-button" href="${item.official_link}" target="_blank">See on Github</a>
-                  </div>
-                </div>  
-              </div>
-            </div>
-            `;
+						if (item.readmeExists) {
+							const baseLink = convertToRawGitHubURL(
+								`${item.official_link}`
+							);
+							result += `
+			<div>
+				<div class="app-card" id="${item.id}">
+					<div class="app-card-container">
+					<a><img href="javascript:void(0)" onclick="openImageModal('${item.image_url}', '${item.name}'); return false" class="app-card-image" src="${item.image_url}" alt="${item.name} image"></a>
+					<h3 class="app-card-name pointer" href="javascript:void(0)" onclick="openReadmeModal('${baseLink}')">${item.name}</h3>
+					<p class="app-card-version">v${item.version}</p>
+					<p class="app-card-author">Created by <a class="app-card-author-link">${item.author}</a></p>
+					<p class="app-card-desc">${item.desc}</p>
+					<div class="app-card-buttons">
+						<a class="app-card-button bold" href="${item.direct_download_link}">Download</a>
+						<a class="app-card-button" href="${item.official_link}" target="_blank">See on Github</a>
+					</div>
+					</div>  
+				</div>
+			</div>
+			`;
+						} else {
+							result += `
+			<div>
+				<div class="app-card" id="${item.id}">
+					<div class="app-card-container">
+					<a><img href="javascript:void(0)" onclick="openImageModal('${item.image_url}', '${item.name}'); return false" class="app-card-image" src="${item.image_url}" alt="${item.name} image"></a>
+					<h3 class="app-card-name">${item.name}</h3>
+					<p class="app-card-version">v${item.version}</p>
+					<p class="app-card-author">Created by <a class="app-card-author-link">${item.author}</a></p>
+					<p class="app-card-desc">${item.desc}</p>
+					<div class="app-card-buttons">
+						<a class="app-card-button bold" href="${item.direct_download_link}">Download</a>
+						<a class="app-card-button" href="${item.official_link}" target="_blank">See on Github</a>
+					</div>
+					</div>  
+				</div>
+			</div>
+			`;
+						}
 					}
 				} else {
 					if (item.official_link == '') {
@@ -117,14 +195,18 @@ class DisplayApps {
             </div>
             `;
 					} else {
-						result += `
+						if (item.readmeExists) {
+							const baseLink = convertToRawGitHubURL(
+								`${item.official_link}`
+							);
+							result += `
             <div>
               <div class="app-card" id="${item.id}">
                 <div class="app-card-container">
                   <a><img href="javascript:void(0)" onclick="openImageModal('${item.image_url}', '${item.name}'); return false" class="app-card-image" src="${item.image_url}" alt="${item.name} image"></a>
-                  <h3 class="app-card-name">${item.name}</h3>
+                  <h3 class="app-card-name pointer" href="javascript:void(0)" onclick="openReadmeModal('${baseLink}')">${item.name}</h3>
                   <p class="app-card-version">v${item.version}</p>
-                  <p class="app-card-author">Created by <a class="app-card-author-link" href="${item.author_link}">${item.author}</a></p>
+                  <p class="app-card-author">Created by <a class="app-card-author-link">${item.author}</a></p>
                   <p class="app-card-desc">${item.desc}</p>
                   <div class="app-card-buttons">
                       <a class="app-card-button bold" href="${item.direct_download_link}">Download</a>
@@ -134,6 +216,25 @@ class DisplayApps {
               </div>
             </div>
             `;
+						} else {
+							result += `
+            <div>
+              <div class="app-card" id="${item.id}">
+                <div class="app-card-container">
+                  <a><img href="javascript:void(0)" onclick="openImageModal('${item.image_url}', '${item.name}'); return false" class="app-card-image" src="${item.image_url}" alt="${item.name} image"></a>
+                  <h3 class="app-card-name">${item.name}</h3>
+                  <p class="app-card-version">v${item.version}</p>
+                  <p class="app-card-author">Created by <a class="app-card-author-link">${item.author}</a></p>
+                  <p class="app-card-desc">${item.desc}</p>
+                  <div class="app-card-buttons">
+                      <a class="app-card-button bold" href="${item.direct_download_link}">Download</a>
+                      <a class="app-card-button" href="${item.official_link}" target="_blank">See on Github</a>
+                  </div>
+                </div>  
+              </div>
+            </div>
+            `;
+						}
 					}
 				}
 			}
@@ -141,6 +242,74 @@ class DisplayApps {
 		appsList.innerHTML = result;
 	}
 }
+
+// ---- README MODAL ----
+
+function openReadmeModal(baseLink) {
+	const modal = document.createElement('div');
+	modal.classList.add('readme-modal');
+	modal.innerHTML = `
+		<div class="readme-modal-container">
+			<div class="readme-modal-close" onclick="closeReadmeModal()">
+				<svg width="50" height="50" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg">
+					<line x1="5" y1="5" x2="25" y2="25" stroke="currentColor" stroke-width="2" />
+					<line x1="25" y1="5" x2="5" y2="25" stroke="currentColor" stroke-width="2" />
+				</svg>
+			</div>
+			<div class="readme-modal-content" id="readme-modal-content">
+				
+			</div>
+		</div>
+		`;
+	document.body.appendChild(modal);
+
+	fetch(`${baseLink}/main/README.md`)
+		.then((response) => response.text())
+		.then((readmeContent) => {
+			const readmeModalContent = document.getElementById(
+				'readme-modal-content'
+			);
+			readmeModalContent.innerHTML = marked.parse(
+				readmeContent
+					.replace(/Images\//g, `${baseLink}/main/Images/`)
+					.replace(/images\//g, `${baseLink}/main/images/`)
+			);
+		})
+		.catch((error) => {
+			console.error('Errore nel caricamento del README:', error);
+			closeReadmeModal();
+		});
+
+	disableScroll();
+
+	document.addEventListener('keydown', function (event) {
+		if (event.key === 'Escape') {
+			closeReadmeModal();
+		}
+	});
+
+	modal.addEventListener('click', function (event) {
+		if (event.target === modal) {
+			closeReadmeModal();
+		}
+	});
+}
+
+function closeReadmeModal() {
+	const modal = document.querySelector('.readme-modal');
+	if (modal) {
+		enableScroll();
+
+		modal.remove();
+		document.removeEventListener('keydown', function (event) {
+			if (event.key === 'Escape') {
+				closeReadmeModal();
+			}
+		});
+	}
+}
+
+// ---- IMAGE MODAL ----
 
 function openImageModal(imageUrl, imageName) {
 	const modal = document.createElement('div');
@@ -157,6 +326,7 @@ function openImageModal(imageUrl, imageName) {
 		</div>
     `;
 	document.body.appendChild(modal);
+
 	disableScroll();
 
 	document.addEventListener('keydown', function (event) {
@@ -176,6 +346,7 @@ function closeImageModal() {
 	const modal = document.querySelector('.image-modal');
 	if (modal) {
 		enableScroll();
+
 		modal.remove();
 		document.removeEventListener('keydown', function (event) {
 			if (event.key === 'Escape') {
@@ -185,49 +356,7 @@ function closeImageModal() {
 	}
 }
 
-var keys = { 32: 1, 33: 1, 34: 1, 35: 1, 36: 1, 37: 1, 38: 1, 39: 1, 40: 1 };
-
-function preventDefault(e) {
-	e.preventDefault();
-}
-
-function preventDefaultForScrollKeys(e) {
-	if (keys[e.keyCode]) {
-		preventDefault(e);
-		return false;
-	}
-}
-
-var supportsPassive = false;
-try {
-	window.addEventListener(
-		'test',
-		null,
-		Object.defineProperty({}, 'passive', {
-			get: function () {
-				supportsPassive = true;
-			},
-		})
-	);
-} catch (e) {}
-
-var wheelOpt = supportsPassive ? { passive: false } : false;
-var wheelEvent =
-	'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
-
-function disableScroll() {
-	window.addEventListener('DOMMouseScroll', preventDefault, false);
-	window.addEventListener(wheelEvent, preventDefault, wheelOpt);
-	window.addEventListener('touchmove', preventDefault, wheelOpt);
-	window.addEventListener('keydown', preventDefaultForScrollKeys, false);
-}
-
-function enableScroll() {
-	window.removeEventListener('DOMMouseScroll', preventDefault, false);
-	window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
-	window.removeEventListener('touchmove', preventDefault, wheelOpt);
-	window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
-}
+// ---- SCROLL ACTIONS ----
 
 function Scroll() {
 	const appElement = document.getElementById(id_query);
@@ -235,6 +364,20 @@ function Scroll() {
 		appElement.scrollIntoView();
 	}
 }
+
+function disableScroll() {
+	var node = document.createElement('style');
+	node.setAttribute('type', 'text/css');
+	node.textContent =
+		'html, body { height: auto ! important ; overflow: hidden ! important ; }';
+	document.head.prepend(node);
+}
+
+function enableScroll() {
+	document.head.querySelector('style')?.remove();
+}
+
+// ---- MAIN ----
 
 document.addEventListener('DOMContentLoaded', () => {
 	const apps = new Apps();
--- js/community-themes.js
+++ js/community-themes.js
@@ -10,6 +10,13 @@ let id_query = params.id;
 
 const themesList = document.getElementById('themesList');
 
+function convertToRawGitHubURL(githubURL) {
+	const rawURL = githubURL
+		.replace('github.com', 'raw.githubusercontent.com')
+		.replace('/blob/', '/');
+	return rawURL;
+}
+
 class Themes {
 	async Items() {
 		try {
@@ -20,13 +27,14 @@ class Themes {
 
 			let themesItems = data.themes;
 
-			themesItems = themesItems.map((item) => {
+			const fetchThemePromises = themesItems.map(async (item) => {
 				const {
 					id,
 					name,
 					author,
 					author_link,
 					desc,
+					version,
 					official_link,
 					direct_download_link,
 					secondary_link,
@@ -34,22 +42,74 @@ class Themes {
 					hidden,
 				} = item.theme;
 
-				return {
-					id,
-					name,
-					author,
-					author_link,
-					desc,
-					official_link,
-					direct_download_link,
-					secondary_link,
-					image_url,
-					hidden,
-				};
+				if (item.theme.official_link == '') {
+					const readmeExists = false;
+					return {
+						id,
+						name,
+						author,
+						author_link,
+						desc,
+						version,
+						official_link,
+						direct_download_link,
+						secondary_link,
+						image_url,
+						hidden,
+						readmeExists,
+					};
+				} else {
+					const rawBaseURL = convertToRawGitHubURL(
+						`${item.theme.official_link}`
+					);
+
+					try {
+						const response = await fetch(
+							`${rawBaseURL}/main/README.md`
+						);
+						const readmeExists = response.status === 200;
+						console.log(readmeExists)
+						return {
+							id,
+							name,
+							author,
+							author_link,
+							desc,
+							version,
+							official_link,
+							direct_download_link,
+							secondary_link,
+							image_url,
+							hidden,
+							readmeExists,
+						};
+					} catch (error) {
+						const readmeExists = false;
+						return {
+							id,
+							name,
+							author,
+							author_link,
+							desc,
+							version,
+							official_link,
+							direct_download_link,
+							secondary_link,
+							image_url,
+							hidden,
+							readmeExists,
+						};
+					}
+				}
 			});
-			return themesItems;
+
+			const themesItemsWithReadme = await Promise.all(fetchThemePromises);
+			console.log("test")
+			console.log(themesItemsWithReadme)
+
+			return themesItemsWithReadme;
 		} catch (error) {
-			console.log(error);
+			// console.log(error);
 		}
 	}
 }
@@ -57,46 +117,199 @@ class Themes {
 class DisplayThemes {
 	displayThemes(themes) {
 		let result = '';
+		
+		console.log(themes);
+
 		themes.forEach((item) => {
 			if (item.hidden != 1) {
 				if (item.author_link == '') {
-					result += `
-          <div>
-            <div class="theme-card" id="${item.id}">
-              <div class="theme-card-container">
-                <a><img href="javascript:void(0)" onclick="openImageModal('${item.image_url}', '${item.name}');  return false" class="theme-card-image" src="${item.image_url}" alt="${item.name} image"></a>
-                <h3 class="theme-card-name">${item.name}</h3>
-                <p class="theme-card-author">Created by <a class="theme-card-author-link">${item.author}</a></p>
-                <p class="theme-card-desc">${item.desc}</p>
-                <div class="theme-card-buttons">
-                    <a class="theme-card-button bold" href="${item.direct_download_link}">Download</a>
-                </div>
-              </div>  
-            </div>
-          </div>
-          `;
+					if (item.official_link == '') {
+						result += `
+			<div>
+				<div class="theme-card" id="${item.id}">
+					<div class="theme-card-container">
+					<a><img href="javascript:void(0)" onclick="openImageModal('${item.image_url}', '${item.name}');  return false" class="theme-card-image" src="${item.image_url}" alt="${item.name} image"></a>
+					<h3 class="theme-card-name">${item.name}</h3>
+					<p class="theme-card-author">Created by <a class="theme-card-author-link">${item.author}</a></p>
+					<p class="theme-card-desc">${item.desc}</p>
+					<div class="theme-card-buttons">
+						<a class="theme-card-button bold" href="${item.direct_download_link}">Download</a>
+					</div>
+					</div>  
+				</div>
+			</div>
+			`;
+					} else {
+						if (item.readmeExists) {
+							const baseLink = convertToRawGitHubURL(
+								`${item.official_link}`
+							);
+							result += `
+			<div>
+				<div class="theme-card" id="${item.id}">
+					<div class="theme-card-container">
+					<a><img href="javascript:void(0)" onclick="openImageModal('${item.image_url}', '${item.name}');  return false" class="theme-card-image" src="${item.image_url}" alt="${item.name} image"></a>
+					<h3 class="theme-card-name pointer" href="javascript:void(0)" onclick="openReadmeModal('${baseLink}')">${item.name}</h3>
+					<p class="theme-card-author">Created by <a class="theme-card-author-link">${item.author}</a></p>
+					<p class="theme-card-desc">${item.desc}</p>
+					<div class="theme-card-buttons">
+						<a class="theme-card-button bold" href="${item.direct_download_link}">Download</a>
+						<a class="theme-card-button" href="${item.official_link}" target="_blank">See on Github</a>
+					</div>
+					</div>  
+				</div>
+			</div>
+			`;
+						} else {
+							result += `
+			<div>
+				<div class="theme-card" id="${item.id}">
+					<div class="theme-card-container">
+					<a><img href="javascript:void(0)" onclick="openImageModal('${item.image_url}', '${item.name}');  return false" class="theme-card-image" src="${item.image_url}" alt="${item.name} image"></a>
+					<h3 class="theme-card-name">${item.name}</h3>
+					<p class="theme-card-author">Created by <a class="theme-card-author-link">${item.author}</a></p>
+					<p class="theme-card-desc">${item.desc}</p>
+					<div class="theme-card-buttons">
+						<a class="theme-card-button bold" href="${item.direct_download_link}">Download</a>
+						<a class="theme-card-button" href="${item.official_link}" target="_blank">See on Github</a>
+					</div>
+					</div>  
+				</div>
+			</div>
+			`;
+						}
+					}
 				} else {
-					result += `
-          <div>
-            <div class="theme-card" id="${item.id}">
-              <div class="theme-card-container">
-                <a><img href="javascript:void(0)" onclick="openImageModal('${item.image_url}', '${item.name}');  return false" class="theme-card-image" src="${item.image_url}" alt="${item.name} image"></a>
-                <h3 class="theme-card-name">${item.name}</h3>
-                <p class="theme-card-author">Created by <a class="theme-card-author-link" href="${item.author_link}">${item.author}</a></p>
-                <p class="theme-card-desc">${item.desc}</p>
-                <div class="theme-card-buttons">
-                    <a class="theme-card-button bold" href="${item.direct_download_link}">Download</a>
-                </div>
-              </div>  
-            </div>
-          </div>
-          `;
+					if (item.official_link == '') {
+						result += `
+			<div>
+				<div class="theme-card" id="${item.id}">
+					<div class="theme-card-container">
+						<a><img href="javascript:void(0)" onclick="openImageModal('${item.image_url}', '${item.name}');  return false" class="theme-card-image" src="${item.image_url}" alt="${item.name} image"></a>
+						<h3 class="theme-card-name">${item.name}</h3>
+						<p class="theme-card-author">Created by <a class="theme-card-author-link" href="${item.author_link}">${item.author}</a></p>
+						<p class="theme-card-desc">${item.desc}</p>
+						<div class="theme-card-buttons">
+							<a class="theme-card-button bold" href="${item.direct_download_link}">Download</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			`;
+					} else {
+						if (item.readmeExists) {
+							const baseLink = convertToRawGitHubURL(
+								`${item.official_link}`
+							);
+							result += `
+			<div>
+				<div class="theme-card" id="${item.id}">
+					<div class="theme-card-container">
+						<a><img href="javascript:void(0)" onclick="openImageModal('${item.image_url}', '${item.name}');  return false" class="theme-card-image" src="${item.image_url}" alt="${item.name} image"></a>
+						<h3 class="theme-card-name pointer" href="javascript:void(0)" onclick="openReadmeModal('${baseLink}')">${item.name}</h3>
+						<p class="theme-card-author">Created by <a class="theme-card-author-link" href="${item.author_link}">${item.author}</a></p>
+						<p class="theme-card-desc">${item.desc}</p>
+						<div class="theme-card-buttons">
+							<a class="theme-card-button bold" href="${item.direct_download_link}">Download</a>
+							<a class="theme-card-button" href="${item.official_link}" target="_blank">See on Github</a>
+						</div>
+					</div>  
+				</div>
+			</div>
+			`;
+						} else {
+							result += `
+			<div>
+				<div class="theme-card" id="${item.id}">
+					<div class="theme-card-container">
+						<a><img href="javascript:void(0)" onclick="openImageModal('${item.image_url}', '${item.name}');  return false" class="theme-card-image" src="${item.image_url}" alt="${item.name} image"></a>
+						<h3 class="theme-card-name">${item.name}</h3>
+						<p class="theme-card-author">Created by <a class="theme-card-author-link" href="${item.author_link}">${item.author}</a></p>
+						<p class="theme-card-desc">${item.desc}</p>
+						<div class="theme-card-buttons">
+							<a class="theme-card-button bold" href="${item.direct_download_link}">Download</a>
+							<a class="theme-card-button" href="${item.official_link}" target="_blank">See on Github</a>
+						</div>
+					</div>  
+				</div>
+			</div>
+			`;
+						}
+					}
 				}
 			}
 		});
 		themesList.innerHTML = result;
 	}
 }
+
+// ---- README MODAL ----
+
+function openReadmeModal(baseLink) {
+	const modal = document.createElement('div');
+	modal.classList.add('readme-modal');
+	modal.innerHTML = `
+		<div class="readme-modal-container">
+			<div class="readme-modal-close" onclick="closeReadmeModal()">
+				<svg width="50" height="50" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg">
+					<line x1="5" y1="5" x2="25" y2="25" stroke="currentColor" stroke-width="2" />
+					<line x1="25" y1="5" x2="5" y2="25" stroke="currentColor" stroke-width="2" />
+				</svg>
+			</div>
+			<div class="readme-modal-content" id="readme-modal-content">
+				
+			</div>
+		</div>
+		`;
+	document.body.appendChild(modal);
+
+	fetch(`${baseLink}/main/README.md`)
+		.then((response) => response.text())
+		.then((readmeContent) => {
+			const readmeModalContent = document.getElementById(
+				'readme-modal-content'
+			);
+			readmeModalContent.innerHTML = marked.parse(
+				readmeContent
+					.replace(/Images\//g, `${baseLink}/main/Images/`)
+					.replace(/images\//g, `${baseLink}/main/images/`)
+			);
+		})
+		.catch((error) => {
+			console.error('Errore nel caricamento del README:', error);
+			closeReadmeModal();
+		});
+
+	disableScroll();
+
+	document.addEventListener('keydown', function (event) {
+		if (event.key === 'Escape') {
+			closeReadmeModal();
+		}
+	});
+
+	modal.addEventListener('click', function (event) {
+		if (event.target === modal) {
+			closeReadmeModal();
+		}
+	});
+}
+
+function closeReadmeModal() {
+	const modal = document.querySelector('.readme-modal');
+	if (modal) {
+		enableScroll();
+
+		modal.remove();
+		document.removeEventListener('keydown', function (event) {
+			if (event.key === 'Escape') {
+				closeReadmeModal();
+			}
+		});
+	}
+}
+
+// ---- IMAGE MODAL ----
 
 function openImageModal(imageUrl, imageName) {
 	const modal = document.createElement('div');
@@ -112,7 +325,8 @@ function openImageModal(imageUrl, imageName) {
             <img src="${imageUrl}" alt="${imageName}">
         </div>
     `;
-	document.body.appendChild(modal);
+	document.body.themeendChild(modal);
+
 	disableScroll();
 
 	document.addEventListener('keydown', function (event) {
@@ -132,6 +346,7 @@ function closeImageModal() {
 	const modal = document.querySelector('.image-modal');
 	if (modal) {
 		enableScroll();
+
 		modal.remove();
 		document.removeEventListener('keydown', function (event) {
 			if (event.key === 'Escape') {
@@ -141,63 +356,34 @@ function closeImageModal() {
 	}
 }
 
-var keys = { 32: 1, 33: 1, 34: 1, 35: 1, 36: 1, 37: 1, 38: 1, 39: 1, 40: 1 };
+// ---- SCROLL ACTIONS ----
 
-function preventDefault(e) {
-	e.preventDefault();
-}
-
-function preventDefaultForScrollKeys(e) {
-	if (keys[e.keyCode]) {
-		preventDefault(e);
-		return false;
+function Scroll() {
+	const themeElement = document.getElementById(id_query);
+	if (themeElement) {
+		themeElement.scrollIntoView();
 	}
 }
 
-var supportsPassive = false;
-try {
-	window.addEventListener(
-		'test',
-		null,
-		Object.defineProperty({}, 'passive', {
-			get: function () {
-				supportsPassive = true;
-			},
-		})
-	);
-} catch (e) {}
-
-var wheelOpt = supportsPassive ? { passive: false } : false;
-var wheelEvent =
-	'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
-
 function disableScroll() {
-	window.addEventListener('DOMMouseScroll', preventDefault, false);
-	window.addEventListener(wheelEvent, preventDefault, wheelOpt);
-	window.addEventListener('touchmove', preventDefault, wheelOpt);
-	window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+	var node = document.createElement('style');
+	node.setAttribute('type', 'text/css');
+	node.textContent =
+		'html, body { height: auto ! important ; overflow: hidden ! important ; }';
+	document.head.prepend(node);
 }
 
 function enableScroll() {
-	window.removeEventListener('DOMMouseScroll', preventDefault, false);
-	window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
-	window.removeEventListener('touchmove', preventDefault, wheelOpt);
-	window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+	document.head.querySelector('style')?.remove();
 }
 
-function Scroll() {
-	const appElement = document.getElementById(id_query);
-	if (appElement) {
-		appElement.scrollIntoView();
-	}
-}
+// ---- MAIN ----
 
 document.addEventListener('DOMContentLoaded', () => {
 	const themes = new Themes();
 	const displaythemes = new DisplayThemes();
 
-	themes
-		.Items()
+	themes.Items()
 		.then((themes) => displaythemes.displayThemes(themes))
 		.then(Scroll);
 });
