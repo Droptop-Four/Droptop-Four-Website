004a0c524256bfbeb035e8b430b1b53dbb00c62b
Bunz <66bunz@gmail.com>
2024-03-07 10:11:02 +0100

    Added supporter downloads counter

--- js/downloads.js
+++ js/downloads.js
@@ -3,18 +3,19 @@
 // ---- DOWNLOAD COUNTER ----
 
 const downloadstag = document.getElementById('download-count');
+const salestag = document.getElementById('sales-count');
 
 async function getDownloads() {
 	try {
 		let result = await fetch('https://api.droptopfour.com/v1/downloads');
 		let data = await result.json();
-		downloads = data.basic_downloads;
+		let downloads = data.basic_downloads;
+		let sales = data.supporter_downloads;
+		downloadstag.innerHTML += `<a>${downloads}</a> downloads `;
+		salestag.innerHTML += `<a>${sales}</a> downloads`;
 	} catch (error) {
 		console.log(error);
 	}
-	document.getElementById(
-		'download-count'
-	).innerHTML += `<a>${downloads}</a> downloads `;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
